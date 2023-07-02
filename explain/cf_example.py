599f85baa5df8d0345eac93667baff26dcb96246
wtaylor17 <wtaylor@upei.ca>
2023-07-02 16:28:56 -0300

    add cf package

--- explain/cf_example.py
+++ explain/cf_example.py
@@ -1,7 +1,12 @@
 from typing import Dict, List, Tuple
 
 import torch
+from tqdm import tqdm
 from pytorch_msssim import ssim
+
+
+def hinge(true, pred):
+    return torch.relu(1 - true * pred)
 
 
 def mse(a: torch.Tensor, b: torch.Tensor):
@@ -47,9 +52,6 @@ class DeepCounterfactualExplainer:
             samples = self.decoder(codes, cf_attrs)
             preds = self.classifier(samples).argmax(1)
 
-            if (preds != target_class).sum() == sample_points:
-                raise ValueError(f"Failed to flip the class label from class {original_class}"
-                                 f" to {target_class}")
             if metric == 'mixture':
                 metric_val = probs
             elif metric == 'mse':
@@ -59,7 +61,108 @@ class DeepCounterfactualExplainer:
                 metric_val = 1 - ssim((xv + 1) / 2, (samples + 1) / 2, data_range=1.0, size_average=False)
             else:
                 raise ValueError(metric)
+            if (preds != target_class).sum() == sample_points:
+                return samples, metric_val
             metric_val = metric_val[preds == target_class]
             samples = samples[preds == target_class]
             sorted_inds = metric_val.argsort()
             return samples[sorted_inds], metric_val[sorted_inds]
+
+
+def max_excluding(y: torch.Tensor, c: int):
+    out = float('-inf')
+    for i in range(y.shape[1]):
+        if i != c and y[:, i].item() > out:
+            out = y[:, i]
+    return out
+
+
+class HingeLossCFExplainer:
+    def __init__(self,
+                 encoder: torch.nn.Module,
+                 decoder: torch.nn.Module,
+                 classifier: torch.nn.Module,
+                 target_feature: str,
+                 latent_dim: int,
+                 categorical_features: List[str] = None,
+                 features_to_ignore: List[str] = None,
+                 c=10.0):
+        self.encoder = encoder
+        self.decoder = decoder
+        self.classifier = classifier
+        self.categorical_features = categorical_features or []
+        self.features_to_ignore = features_to_ignore or []
+        self.c = c
+        self.target_feature = target_feature
+        self.latent_dim = latent_dim
+
+    def explain(self, x: torch.Tensor,
+                attrs: Dict[str, torch.Tensor],
+                target_class=None,
+                train_z=True,
+                steps=30,
+                lr=0.1):
+
+        codes = self.encoder(x, attrs).detach()
+        with torch.no_grad():
+            original_pred = self.classifier(x).softmax(1)
+            original_class = original_pred.argmax(1).item()
+
+        def hinge(x_):
+            pred = self.classifier(x_)
+
+            if target_class is not None:
+                return (max_excluding(pred, target_class) - pred[:, target_class]).mean()
+            return (pred - original_pred).square().mean()
+
+        def total_loss(x_):
+            # c = self.c if self.classifier(x_).argmax(1).item() != target_class else 0
+            h = hinge(x_)
+            m = (x - x_).abs().mean()
+            return self.c * h + m, h, m
+
+        params = {
+            k: torch.nn.Parameter(0.01 * torch.randn((1, attrs[k].shape[1])),
+                                  requires_grad=True)
+            for k in attrs
+            if k not in self.features_to_ignore
+        }
+        if train_z:
+            params["z"] = torch.randn(codes.shape)
+
+        opt = torch.optim.Adam(list(params.values()), lr=lr)
+
+        tq = tqdm(list(range(steps)))
+        for _ in tq:
+            opt.zero_grad()
+            attrs_cf = dict(**params)
+            if train_z:
+                del attrs_cf["z"]
+            for k in attrs:
+                if k in self.features_to_ignore:
+                    attrs_cf[k] = attrs[k]
+                elif k in self.categorical_features:
+                    attrs_cf[k] = attrs_cf[k].softmax(1)
+                else:
+                    attrs_cf[k] = attrs_cf[k].tanh()
+            z = params["z"].tanh() if train_z else codes
+            x_cf = self.decoder(z, attrs_cf)
+            loss, cls, rec = total_loss(x_cf)
+            loss.backward()
+            opt.step()
+            tq.set_postfix(rec=rec.detach().item(), cls=cls.detach().item())
+
+        attrs_cf = dict(**params)
+        if train_z:
+            del attrs_cf["z"]
+        for k in attrs:
+            if k in self.features_to_ignore:
+                attrs_cf[k] = attrs[k]
+            elif k in self.categorical_features:
+                attrs_cf[k] = attrs_cf[k].softmax(1)
+            else:
+                attrs_cf[k] = attrs_cf[k].tanh()
+        z = params["z"].tanh() if train_z else codes
+        x_cf = self.decoder(z, attrs_cf)
+
+        return x_cf
