d5049fe33d4f66a3c8e94e1656d8c8fce4bb9d95
wtaylor17 <wtaylor@upei.ca>
2023-07-12 14:14:46 -0300

    measured cf script

--- mnist_gan_measured_cf.py
+++ mnist_gan_measured_cf.py
@@ -7,117 +7,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from image_scms import mnist as image_mnist
-from attribute_scms import mnist as attribute_mnist
-from attribute_scms.training_utils import nf_forward, nf_inverse
+from attribute_scms.mnist import GroundTruthCausalGraph, MNISTCausalGraph
 from tqdm import tqdm
 from morphomnist.morpho import ImageMorphology, ImageMoments
-from morphomnist.perturb import SetThickness, SetSlant
-
-
-def sigmoid_inverse(x_):
-    return torch.log(x_) - torch.log(1 - x_)
-
-
-class MorphoMNISTSCM(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.t_noise_dist = torch.distributions.Gamma(10, 5)
-        self.i_noise_dist = torch.distributions.Normal(0, 1)
-        self.s_noise_dist = torch.distributions.Normal(0, 0.1)
-
-    def generate_t(self, n=1, noise=None):
-        if noise is None:
-            et = self.t_noise_dist.sample((n, 1))
-        else:
-            et = noise
-        return (et + 0.5).float()
-
-    def generate_s(self, n=1, noise=None):
-        if noise is None:
-            es = self.s_noise_dist.sample((n, 1))
-        else:
-            es = noise
-        return np.pi * es
-
-    def generate_i(self, t, noise=None):
-        if noise is None:
-            ei = torch.randn(t.shape)
-        else:
-            ei = noise
-        return 191 * torch.sigmoid(.5 * ei + 2 * t - 5) + 64
-
-    def generate(self, n=1):
-        t = self.generate_t(n)
-        i_ = self.generate_i(t)
-        s = self.generate_s(n)
-        return t, i_, s
-
-    def counterfactual_t(self, x_, a_, t_new_):
-        cls = a_[:, :10]
-        t_, i_, s_ = a_[:, 10], a_[:, 11], a_[:, 12]
-        i_noise = 2 * (sigmoid_inverse((i_ - 64) / 191) + 5 - 2 * t_)
-        i_out = self.generate_i(t_new_, noise=i_noise.reshape((-1, 1)))
-        attrs_out = torch.concat([cls.reshape((-1, 10)), t_new_.reshape((-1, 1)),
-                                  i_out.reshape((-1, 1)),
-                                  s_.reshape((-1, 1))], dim=1)
-        imgs_out = np.zeros((x_.size(0), 1, 28, 28))
-        for idx, (im_, th_, in_, sl_) in tqdm(enumerate(zip(x_.reshape((-1, 28, 28)).cpu().numpy(),
-                                                            t_new_.flatten().cpu().numpy(),
-                                                            i_out.flatten().cpu().numpy(),
-                                                            s_.cpu().numpy()))):
-            morph = ImageMorphology(im_.reshape((28, 28)), scale=16)
-            new_img = np.float32(SetThickness(th_)(morph))
-            new_img = morph.downscale(np.float32(SetSlant(sl_)(ImageMorphology(new_img))))
-            img_min, img_max = new_img.min(), new_img.max()
-            current_intensity = np.median(new_img[new_img >= img_min + (img_max - img_min) * .5])
-            mult = in_ / current_intensity
-            new_img = np.clip(new_img * mult, 0, 255)
-            imgs_out[idx, 0, :, :] = new_img
-        return torch.from_numpy(imgs_out), attrs_out
-
-    def counterfactual_i(self, x_, a_, i_new_):
-        cls = a_[:, :10]
-        t_, i_, s_ = a_[:, 10], a_[:, 11], a_[:, 12]
-        i_out = i_new_
-        attrs_out = torch.concat([cls.reshape((-1, 10)), t_.reshape((-1, 1)),
-                                  i_out.reshape((-1, 1)),
-                                  s_.reshape((-1, 1))], dim=1)
-        imgs_out = np.zeros((x_.size(0), 1, 28, 28))
-        for idx, (im_, th_, in_, sl_) in tqdm(enumerate(zip(x_.reshape((-1, 28, 28)).cpu().numpy(),
-                                                            t_.flatten().cpu().numpy(),
-                                                            i_out.flatten().cpu().numpy(),
-                                                            s_.cpu().numpy()))):
-            morph = ImageMorphology(im_.reshape((28, 28)), scale=16)
-            new_img = np.float32(SetThickness(th_)(morph))
-            new_img = morph.downscale(np.float32(SetSlant(sl_)(ImageMorphology(new_img))))
-            img_min, img_max = new_img.min(), new_img.max()
-            current_intensity = np.median(new_img[new_img >= img_min + (img_max - img_min) * .5])
-            mult = in_ / current_intensity
-            new_img = np.clip(new_img * mult, 0, 255)
-            imgs_out[idx, 0, :, :] = new_img
-        return torch.from_numpy(imgs_out), attrs_out
-
-    def counterfactual_s(self, x_, a_, s_new_):
-        cls = a_[:, :10]
-        t_, i_, s_ = a_[:, 10], a_[:, 11], a_[:, 12]
-        s_out = s_new_
-        attrs_out = torch.concat([cls.reshape((-1, 10)), t_.reshape((-1, 1)),
-                                  i_.reshape((-1, 1)),
-                                  s_out.reshape((-1, 1))], dim=1)
-        imgs_out = np.zeros((x_.size(0), 1, 28, 28))
-        for idx, (im_, th_, in_, sl_) in tqdm(enumerate(zip(x_.reshape((-1, 28, 28)).cpu().numpy(),
-                                                            t_.flatten().cpu().numpy(),
-                                                            i_.flatten().cpu().numpy(),
-                                                            s_out.cpu().numpy()))):
-            morph = ImageMorphology(im_.reshape((28, 28)), scale=16)
-            new_img = np.float32(SetThickness(th_)(morph))
-            new_img = morph.downscale(np.float32(SetSlant(sl_)(ImageMorphology(new_img))))
-            img_min, img_max = new_img.min(), new_img.max()
-            current_intensity = np.median(new_img[new_img >= img_min + (img_max - img_min) * .5])
-            mult = in_ / current_intensity
-            new_img = np.clip(new_img * mult, 0, 255)
-            imgs_out[idx, 0, :, :] = new_img
-        return torch.from_numpy(imgs_out), attrs_out
 
 
 def extract_observed_attributes(image):
@@ -129,7 +21,7 @@ def extract_observed_attributes(image):
     img_min, img_max = image.min(), image.max()
     intensity = np.median(image[image >= img_min + (img_max - img_min) * .5])
     moments = ImageMoments(image)
-    slant = moments.horizontal_shear
+    slant = -moments.horizontal_shear
     return np.array([thickness, intensity, slant])
 
 
@@ -137,15 +29,15 @@ parser = ArgumentParser()
 parser.add_argument('--data-dir', type=str,
                     help='path to folder with .npy files of data',
                     default='')
-parser.add_argument('--image-model-file',
+parser.add_argument('--image-model',
                     type=str,
                     help='file (.tar) for saved cnn models')
-parser.add_argument('--attr-model-file',
+parser.add_argument('--attribute-model',
                     type=str,
                     help='file (.tar) for saved attribute models')
-parser.add_argument('--cf-attribute',
+parser.add_argument('--model-name',
                     type=str,
-                    default='thickness')
+                    default='ImageCFGen')
 
 np.random.seed(42)
 torch.manual_seed(42)
@@ -160,98 +52,82 @@ if __name__ == '__main__':
     )).float().to(device)
     a_test = torch.from_numpy(np.load(
         os.path.join(args.data_dir, 'mnist-a-test.npy')
-    )).float().to(device)[:1000]
-    x_train = torch.from_numpy(np.load(
-        os.path.join(args.data_dir, 'mnist-x-train.npy')
     )).float().to(device)
     x_test = torch.from_numpy(np.load(
         os.path.join(args.data_dir, 'mnist-x-test.npy')
-    )).float().to(device)[:1000]
+    )).float().to(device)
 
-    ground_truth_scm = MorphoMNISTSCM()
+    a_train = {
+        "digit": a_train[:, :10].float(),
+        "thickness": a_train[:, 10:11].float(),
+        "intensity": a_train[:, 11:12].float(),
+        "slant": a_train[:, 12:13].float()
+    }
+    a_test = {
+        "digit": a_test[:, :10].float(),
+        "thickness": a_test[:, 10:11].float(),
+        "intensity": a_test[:, 11:12].float(),
+        "slant": a_test[:, 12:13].float()
+    }
 
-    E, G, D = image_mnist.load_model(args.image_model_file,
-                                     device=device)
-    E = E.to(device)
-    G = G.to(device)
-    D = D.to(device)
+    attr_stats = {
+        k: (v.min(dim=0).values, v.max(dim=0).values)
+        for k, v in a_train.items()
+        if k != "digit"
+    }
 
-    t_dist, i_given_t_dist, s_dist = attribute_mnist.load_model(args.attr_model_file,
-                                                                device=device)
+    ground_truth_scm = GroundTruthCausalGraph()
+    trained_graph: MNISTCausalGraph = torch.load(args.attribute_model, map_location=device)['graph']
+    trained_graph.get_module('thickness').eval()
 
-    n_show = 10
-    inds = np.random.permutation(len(x_test))
-    D.eval()
-    E.eval()
-    G.eval()
+    model_dict = torch.load(args.image_model, map_location=device)
+    if 'vae' in args.image_model:
+        generator = model_dict["vae"].decoder
+        encoder = lambda *arg: model_dict["vae"].encoder(*arg)[0]
+    else:
+        generator = model_dict["G"]
+        encoder = model_dict["E"]
+
     with torch.no_grad():
-        x = x_test.reshape((-1, 1, 28, 28)).to(device) / 255.0
-        c = torch.clone(a_test.reshape((-1, 13)))
-        c_raw = a_test.reshape((-1, 13))
-        c_min, c_max = a_train[:, 10:].min(dim=0).values, a_train[:, 10:].max(dim=0).values
-        c[:, 10:] = (c[:, 10:] - c_min) / (c_max - c_min)
+        x_test = 2 * x_test.reshape((-1, 1, 28, 28)).to(device) / 255.0 - 1
+        fig, axs = plt.subplots(1, 3)
+        for i, attribute in enumerate(['thickness', 'intensity', 'slant']):
+            cf_int = {
+                attribute: ground_truth_scm.sample(n=len(x_test))[attribute].reshape((-1, 1))
+            }
+            cf_real = ground_truth_scm.sample_cf(a_test, cf_int)
+            cf_approx = trained_graph.sample_cf(a_test, cf_int)
 
-        if args.cf_attribute == 'thickness':
-            i_noise_pred = nf_inverse(i_given_t_dist.condition(c_raw[:, 10:11]),
-                                      c_raw[:, 11:12])
-            t_new = t_dist.sample((len(c_raw),))
-            i_new = nf_forward(i_given_t_dist.condition(t_new), i_noise_pred)
+            a_test_scaled = {
+                k: 2 * (v - attr_stats[k][0]) / (attr_stats[k][1] - attr_stats[k][0]) - 1
+                for k, v in a_test.items()
+                if k != "digit"
+            }
+            a_test_scaled["digit"] = a_test["digit"]
+            codes = encoder(x_test, a_test_scaled)
 
-            true_image_out, true_new_attributes = ground_truth_scm.counterfactual_t(x_test, c_raw, t_new)
-            attr_idx = 0
-            s_new = c_raw[:, 12:13]
-        elif args.cf_attribute == 'intensity':
-            t_new = c_raw[:, 10:11]
-            s_new = c_raw[:, 12:13]
-            i_new = i_given_t_dist.condition(t_new).sample((len(c_raw),))
-            attr_idx = 1
-            true_image_out, true_new_attributes = ground_truth_scm.counterfactual_i(x_test, c_raw, i_new)
-        elif args.cf_attribute == 'slant':
-            t_new = c_raw[:, 10:11]
-            i_new = c_raw[:, 11:12]
-            s_new = s_dist.sample((len(c_raw),))
-            attr_idx = 2
-            true_image_out, true_new_attributes = ground_truth_scm.counterfactual_s(x_test, c_raw, s_new)
-        else:
-            raise ValueError
+            cf_approx_scaled = {
+                k: 2 * (v - attr_stats[k][0]) / (attr_stats[k][1] - attr_stats[k][0]) - 1
+                for k, v in cf_approx.items()
+                if k != "digit"
+            }
+            cf_approx_scaled["digit"] = cf_approx["digit"]
+            pred_image_out = generator(codes, cf_approx_scaled)
 
-        measured_real = np.row_stack([
-            extract_observed_attributes(true_image)
-            for true_image in tqdm(true_image_out)
-        ])
+            measured_pred = np.row_stack([
+                extract_observed_attributes(255 * (pred_image + 1) / 2)
+                for pred_image in tqdm(pred_image_out, total=len(x_test))
+            ])
 
-        codes = E(x, c)
-        c_cf = torch.clone(c_raw)
-        c_cf[:, 10] = t_new.flatten()
-        c_cf[:, 11] = i_new.flatten()
-        c_cf[:, 12] = s_new.flatten()
-        c_cf[:, 10:] = (c_cf[:, 10:] - c_min) / (c_max - c_min)
-        pred_image_out = G(codes, c_cf)
+            a_range = torch.range(float(attr_stats[attribute][0]) - 0.2,
+                                  float(attr_stats[attribute][1]) + 0.2, 0.01)
 
-        measured_pred = np.row_stack([
-            extract_observed_attributes(255 * pred_image)
-            for pred_image in tqdm(pred_image_out)
-        ])
+            axs[i].plot(a_range, a_range, 'k--')
 
-        a_range = torch.range(float(a_train[:, 10 + attr_idx].min().item()) - 1,
-                              float(a_train[:, 10 + attr_idx].max().item()) + 1, 0.01)
-
-        fig, axs = plt.subplots(1, 2)
-        axs[0].plot(a_range, a_range, 'k--')
-        axs[1].plot(a_range, a_range, 'k--')
-
-        axs[0].scatter(true_new_attributes[:, 10 + attr_idx].cpu().numpy(),
-                       measured_pred[:, attr_idx])
-        axs[0].set_ylabel('ImageCFGen Measured')
-        axs[0].set_xlabel('Target')
-        axs[1].scatter(true_new_attributes[:, 10 + attr_idx].cpu().numpy(),
-                       measured_real[:, attr_idx])
-        axs[1].set_ylabel('REAL Measured')
-        axs[1].set_xlabel('Target')
-        fig.suptitle(args.cf_attribute.capitalize())
-
+            axs[i].scatter(cf_real[attribute].cpu().numpy(),
+                           measured_pred[:, i], c='blue', alpha=0.7)
+            axs[i].set_xlabel('Target value')
+            axs[i].set_ylabel('Measured value')
+            axs[i].set_title(attribute.capitalize())
+        fig.suptitle(args.model_name + ' Morpho-MNIST CF attribute measurement')
         plt.show()
-
-        pred_error = torch.abs(true_new_attributes[:, -3:] - measured_pred)
-
-        print(f'do({args.cf_attribute}) median absolute error:', pred_error[:, attr_idx].median().item())
