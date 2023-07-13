e5b1e7f42991a4d7fbaf4a1f0e56e921828c2559
wtaylor17 <wtaylor@upei.ca>
2023-07-13 11:33:15 -0300

    update to mnist generator score script

--- mnist_generator_score.py
+++ mnist_generator_score.py
@@ -13,25 +13,29 @@ parser.add_argument('--data-dir', type=str,
                     help='path to folder with .npy files of data',
                     default='')
 parser.add_argument("-m", "--image-model", type=str)
-parser.add_argument("-a", "--attribute-model", type=str)
 parser.add_argument("-c", "--classifier", type=str)
-parser.add_argument("-n", "--num-samples", type=int, default=10_000)
 
 if __name__ == '__main__':
     sns.set()
     args = parser.parse_args()
     model_path = args.image_model
-    attr_path = args.attribute_model
-    num_samples = args.num_samples
     clf_path = args.classifier
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    generator: mnist.Generator = torch.load(model_path, map_location=device)["G"].to(device)
-    attrs_graph: MNISTCausalGraph = torch.load(attr_path, map_location=device)["graph"]
+    model_dict = torch.load(model_path, map_location=device)
+    if 'vae' in model_path:
+        generator = model_dict["vae"].decoder
+        encoder = lambda *arg: model_dict["vae"].encoder(*arg)[0]
+    else:
+        generator = model_dict["G"]
+        encoder = model_dict["E"]
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     a_train = torch.from_numpy(np.load(
         os.path.join(args.data_dir, 'mnist-a-train.npy')
+    )).float().to(device)
+    a_test = torch.from_numpy(np.load(
+        os.path.join(args.data_dir, 'mnist-a-test.npy')
     )).float().to(device)
 
     a_train = {
@@ -47,22 +51,26 @@ if __name__ == '__main__':
         if k != "digit"
     }
 
+    a_test = {
+        "digit": a_test[:, :10].float(),
+        "thickness": a_test[:, 10:11].float(),
+        "intensity": a_test[:, 11:12].float(),
+        "slant": a_test[:, 12:13].float()
+    }
+
     digit_clf = torch.load(clf_path, map_location=device)["clf"]
 
     with torch.no_grad():
-        generator.eval()
-
         digit_acc = 0
-        attrs = attrs_graph.sample(n=num_samples)
-        attrs["digit"] = torch.eye(10)[attrs["digit"].flatten()]
-        for k in attr_stats:
-            attrs[k] = 2 * (attrs[k] - attr_stats[k][0]) / (attr_stats[k][1] - attr_stats[k][0]) - 1
 
-        for attr_batch in batchify_dict(attrs):
+        for k in attr_stats:
+            a_test[k] = 2 * (a_test[k] - attr_stats[k][0]) / (attr_stats[k][1] - attr_stats[k][0]) - 1
+
+        for attr_batch in batchify_dict(a_test):
             z = torch.randn(size=(len(attr_batch["digit"]), mnist.LATENT_DIM, 1, 1))
             gen = generator(z, attr_batch)
             preds = digit_clf(gen).argmax(1)
             true = attr_batch["digit"].argmax(1)
             digit_acc += (true == preds).sum().cpu().item()
 
-        print("Digit accuracy (generated):", digit_acc / num_samples)
+        print("Digit accuracy (generated):", digit_acc / len(a_test["digit"]))
