d8e0d543d6439f20d4c79ac70cc3b0edf346ab4c
wtaylor17 <wtaylor@upei.ca>
2023-07-13 11:32:45 -0300

    cf figure script for mnist

--- mnist_gan_counterfactuals.py
+++ mnist_gan_counterfactuals.py
@@ -1,176 +1,93 @@
 from argparse import ArgumentParser
 import os
-
 import torch
-import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-from image_scms import mnist as image_mnist
-from attribute_scms import mnist as attribute_mnist
 from attribute_scms.mnist import MNISTCausalGraph
-from tqdm import tqdm
-from morphomnist.morpho import ImageMorphology
-from morphomnist.perturb import SetThickness, SetSlant
-
-
-def sigmoid_inverse(x_):
-    return torch.log(x_) - torch.log(1 - x_)
-
-
-class MorphoMNISTSCM(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.t_noise_dist = torch.distributions.Gamma(10, 5)
-        self.i_noise_dist = torch.distributions.Normal(0, 1)
-        self.s_noise_dist = torch.distributions.Normal(0, 0.1)
-
-    def generate_t(self, n=1, noise=None):
-        if noise is None:
-            et = self.t_noise_dist.sample((n, 1))
-        else:
-            et = noise
-        return (et + 0.5).float()
-
-    def generate_s(self, n=1, noise=None):
-        if noise is None:
-            es = self.s_noise_dist.sample((n, 1))
-        else:
-            es = noise
-        return np.pi * es
-
-    def generate_i(self, t, noise=None):
-        if noise is None:
-            ei = torch.randn(t.shape)
-        else:
-            ei = noise
-        return 191 * torch.sigmoid(.5 * ei + 2 * t - 5) + 64
-
-    def generate(self, n=1):
-        t = self.generate_t(n)
-        i_ = self.generate_i(t)
-        s = self.generate_s(n)
-        return t, i_, s
-
-    def counterfactual_t(self, x_, a_, t_new_):
-        cls = a_[:, :10]
-        t_, i_, s_ = a_[:, 10], a_[:, 11], a_[:, 12]
-        i_noise = 2 * (sigmoid_inverse((i_ - 64) / 191) + 5 - 2 * t_)
-        i_out = self.generate_i(t_new_, noise=i_noise.reshape((-1, 1)))
-        attrs_out = torch.concat([cls.reshape((-1, 10)), t_new_.reshape((-1, 1)),
-                                  i_out.reshape((-1, 1)),
-                                  s_.reshape((-1, 1))], dim=1)
-        imgs_out = np.zeros((x_.size(0), 1, 28, 28))
-        for idx, (im_, th_, in_, sl_) in tqdm(enumerate(zip(x_.reshape((-1, 28, 28)).cpu().numpy(),
-                                                            t_new_.flatten().cpu().numpy(),
-                                                            i_out.flatten().cpu().numpy(),
-                                                            s_.cpu().numpy()))):
-            morph = ImageMorphology(im_.reshape((28, 28)), scale=16)
-            new_img = np.float32(SetThickness(th_)(morph))
-            new_img = morph.downscale(np.float32(SetSlant(sl_)(ImageMorphology(new_img))))
-            img_min, img_max = new_img.min(), new_img.max()
-            current_intensity = np.median(new_img[new_img >= img_min + (img_max - img_min) * .5])
-            mult = in_ / current_intensity
-            new_img = np.clip(new_img * mult, 0, 255)
-            imgs_out[idx, 0, :, :] = new_img
-        return torch.from_numpy(imgs_out), attrs_out
 
 
 parser = ArgumentParser()
 parser.add_argument('--data-dir', type=str,
                     help='path to folder with .npy files of data',
                     default='')
-parser.add_argument('--image-model-file',
-                    type=str,
-                    help='file (.tar) for saved cnn models')
-parser.add_argument('--attr-model-file',
-                    type=str,
-                    help='file (.tar) for saved attribute models')
 
 if __name__ == '__main__':
-    sns.set()
+    np.random.seed(42)
     args = parser.parse_args()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    vae = torch.load('mnist-vae.tar', map_location=device)["vae"]
+    bigan_model_dict = torch.load('mnist-bigan.tar', map_location=device)
+    bigan_ft_model_dict = torch.load('mnist-bigan-finetuned-mse.tar', map_location=device)
+
     a_train = torch.from_numpy(np.load(
         os.path.join(args.data_dir, 'mnist-a-train.npy')
     )).float().to(device)
     a_test = torch.from_numpy(np.load(
         os.path.join(args.data_dir, 'mnist-a-test.npy')
-    )).float().to(device)[:20]
-    x_train = torch.from_numpy(np.load(
-        os.path.join(args.data_dir, 'mnist-x-train.npy')
     )).float().to(device)
-    x_test = torch.from_numpy(np.load(
+    x_test = 2 * torch.from_numpy(np.load(
         os.path.join(args.data_dir, 'mnist-x-test.npy')
-    )).float().to(device)[:20]
+    )).float().to(device).reshape((-1, 1, 28, 28)) / 255.0 - 1
 
-    ground_truth_scm = MorphoMNISTSCM()
+    graph: MNISTCausalGraph = torch.load('mnist-attribute-scm.tar', map_location=device)["graph"]
+    graph.get_module('thickness').eval()
 
-    E, G, D = image_mnist.load_model(args.image_model_file,
-                                     device=device)
-    E = E.to(device)
-    G = G.to(device)
-    D = D.to(device)
+    ind = np.random.permutation(len(x_test))[0]
+    a_train = {
+        "digit": a_train[:, :10].float(),
+        "thickness": a_train[:, 10:11].float(),
+        "intensity": a_train[:, 11:12].float(),
+        "slant": a_train[:, 12:13].float()
+    }
+    attr_stats = {
+        k: (v.min(dim=0).values, v.max(dim=0).values)
+        for k, v in a_train.items()
+        if k != "digit"
+    }
 
-    graph: MNISTCausalGraph = torch.load(args.attr_model_file)["graph"]
+    x_test = x_test[ind:ind + 1]
 
-    n_show = 10
-    inds = np.random.permutation(len(x_test))
-    D.eval()
-    E.eval()
-    G.eval()
+    a_test = {
+        "digit": a_test[ind:ind+1, :10].float(),
+        "thickness": a_test[ind:ind+1, 10:11].float(),
+        "intensity": a_test[ind:ind+1, 11:12].float(),
+        "slant": a_test[ind:ind+1, 12:13].float()
+    }
+    a_test_scaled = {"digit": a_test["digit"]}
+    for k in attr_stats:
+        if k != "digit":
+            a_test_scaled[k] = 2 * (a_test[k] - attr_stats[k][0]) / (attr_stats[k][1] - attr_stats[k][0]) - 1
+
     with torch.no_grad():
-        x = x_test.reshape((-1, 1, 28, 28)).to(device) / 255.0
-        c = torch.clone(a_test.reshape((-1, 13)))
-        c_raw = a_test.reshape((-1, 13))
-        c_min, c_max = a_train[:, 10:].min(dim=0).values, a_train[:, 10:].max(dim=0).values
-        c[:, 10:] = (c[:, 10:] - c_min) / (c_max - c_min)
+        t_cf = {"thickness": a_test["thickness"] + 2}
+        new_attrs = graph.sample_cf(a_test, t_cf)
+        a_cf_scaled = {"digit": new_attrs["digit"]}
+        for k in attr_stats:
+            if k != "digit":
+                a_cf_scaled[k] = 2 * (new_attrs[k] - attr_stats[k][0]) / (attr_stats[k][1] - attr_stats[k][0]) - 1
+        print(a_test_scaled)
+        print(a_cf_scaled)
+        bigan_cf = bigan_model_dict["G"](bigan_model_dict["E"](x_test, a_test_scaled), a_cf_scaled)
+        vae_cf = vae.decoder(vae.encoder(x_test, a_test_scaled)[0], a_cf_scaled)
+        bigan_ft_cf = bigan_ft_model_dict["G"](bigan_ft_model_dict["E"](x_test, a_test_scaled), a_cf_scaled)
 
-        t_new = c_raw[:, 10:11] + 2
-
-        cf = graph.sample_cf({
-            "label": c[:, 10:].argmax(1),
-            "thickness": c_raw[:, 10:11],
-            "intensity": c_raw[:, 11:12],
-            "slant": c_raw[:, 12:13]
-        }, {
-            "thickness": t_new
-        })
-
-        true_image_out, true_new_attributes = ground_truth_scm.counterfactual_t(x_test, c_raw, t_new)
-
-        codes = E(x, c)
-        c_cf = torch.clone(c_raw)
-        c_cf[:, 10] = t_new.flatten()
-        c_cf[:, 11] = cf["intensity"].flatten()
-        c_cf[:, 10:] = (c_cf[:, 10:] - c_min) / (c_max - c_min)
-        pred_image_out = G(codes, c_cf)
-
-        real = x_test.reshape((-1, 28, 28)).cpu().numpy() / 255.0
-        true_image_out = true_image_out / 255.0
-
-        mae = torch.abs(true_image_out - pred_image_out).mean()
-        print('MAE (pixel):', 255 * mae.item())
-
-        fig, ax = plt.subplots(3, n_show, figsize=(15, 5))
-        fig.subplots_adjust(wspace=0.05, hspace=0)
-        plt.rcParams.update({'font.size': 20})
-        fig.suptitle('ImageCFGen Morpho-MNIST Thickness Counterfactuals')
-        fig.text(0.01, 0.75, 'Original', ha='left')
-        fig.text(0.01, 0.5, 'do(t+2) GT', ha='left')
-        fig.text(0.01, 0.25, 'do(t+2) pred', ha='left')
-
-        for i in range(n_show):
-            j = inds[i]
-            ax[0, i].imshow(real[j], cmap='gray', vmin=0, vmax=1)
-            # ax[0, i].set_title(
-            #     f'c = {c_raw[j, :10].argmax().item()}, t = {round(float(c_raw[j, 10].item()), 2)}\ni'
-            #     f' = {round(float(c_raw[j, 11].item()), 2)}, s = {round(float(c_raw[j, 12].item()), 2)}',
-            #     fontsize=8)
-            ax[0, i].axis('off')
-            ax[1, i].imshow(true_image_out[j].reshape((28, 28)), cmap='gray', vmin=0, vmax=1)
-            ax[1, i].axis('off')
-            ax[2, i].imshow(pred_image_out[j].reshape((28, 28)), cmap='gray', vmin=0, vmax=1)
-            ax[2, i].axis('off')
+        fig, axs = plt.subplots(1, 4)
+        axs[0].imshow(x_test.reshape((28, 28)), vmin=-1, vmax=1, cmap="gray")
+        axs[0].set_xticks([])
+        axs[0].set_yticks([])
+        axs[0].set_title('Original')
+        axs[1].imshow(bigan_cf.reshape((28, 28)), vmin=-1, vmax=1, cmap="gray")
+        axs[1].set_xticks([])
+        axs[1].set_yticks([])
+        axs[1].set_title('ImageCFGen')
+        axs[2].imshow(bigan_ft_cf.reshape((28, 28)), vmin=-1, vmax=1, cmap="gray")
+        axs[2].set_xticks([])
+        axs[2].set_yticks([])
+        axs[2].set_title('ImageCFGen (fine-tuned)')
+        axs[3].imshow(vae_cf.reshape((28, 28)), vmin=-1, vmax=1, cmap="gray")
+        axs[3].set_xticks([])
+        axs[3].set_yticks([])
+        axs[3].set_title('DeepSCM')
+        fig.suptitle('Morpho-MNIST do(t+2) counterfactuals')
         plt.show()
