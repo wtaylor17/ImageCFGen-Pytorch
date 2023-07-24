cb7dd42e5ce152ce24a099ac91fa828bdbbcea6a
wtaylor17 <wtaylor@upei.ca>
2023-07-24 15:40:01 -0300

    forgot argmax

--- audiomnist_cf_classifier_metric.py
+++ audiomnist_cf_classifier_metric.py
@@ -90,11 +90,11 @@ if __name__ == "__main__":
                 bigan_int = G(torch.randn_like(bigan_codes), cf_a)
                 vae_int = vae.decoder(torch.randn_like(vae_codes), cf_a)
 
-                bigan_mat[subject, d] = (subject_clf(bigan_cf) == d).int().cpu().numpy()
-                bigan_ft_mat[subject, d] = (subject_clf(bigan_ft_cf) == d).int().cpu().numpy()
-                vae_mat[subject, d] = (subject_clf(vae_cf) == d).int().cpu().numpy()
-                bigan_int_mat[subject, d] = (subject_clf(bigan_int) == d).int().cpu().numpy()
-                vae_int_mat[subject, d] = (subject_clf(vae_int) == d).int().cpu().numpy()
+                bigan_mat[subject, d] = (subject_clf(bigan_cf).argmax(1) == d).int().cpu().numpy()
+                bigan_ft_mat[subject, d] = (subject_clf(bigan_ft_cf).argmax(1) == d).int().cpu().numpy()
+                vae_mat[subject, d] = (subject_clf(vae_cf).argmax(1) == d).int().cpu().numpy()
+                bigan_int_mat[subject, d] = (subject_clf(bigan_int).argmax(1) == d).int().cpu().numpy()
+                vae_int_mat[subject, d] = (subject_clf(vae_int).argmax(1) == d).int().cpu().numpy()
 
     np.save('bigan_cf_agreement_mat.npy', bigan_mat)
     np.save('bigan_ft_cf_agreement_mat.npy', bigan_ft_mat)
