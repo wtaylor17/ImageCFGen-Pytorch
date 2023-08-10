b9373a9a86e16668aa4f0321680576c0b8627324
wtaylor17 <wtaylor@upei.ca>
2023-08-10 12:47:08 -0300

    trying to fix something

--- audiomnist_cf_classifier_metric.py
+++ audiomnist_cf_classifier_metric.py
@@ -91,14 +91,14 @@ if __name__ == "__main__":
                 bigan_int = G(torch.randn_like(bigan_codes), cf_a)
                 vae_int = vae.decoder(torch.randn_like(vae_codes), cf_a)
 
-                bigan_mat[subject-1, d] = (
+                bigan_mat[subject, d] = (
                             subject_clf(bigan_cf).argmax(1) == subject).int().cpu().numpy()
-                bigan_ft_mat[subject-1, d] = (
+                bigan_ft_mat[subject, d] = (
                             subject_clf(bigan_ft_cf).argmax(1) == subject).int().cpu().numpy()
-                vae_mat[subject-1, d] = (subject_clf(vae_cf).argmax(1) == subject-1).int().cpu().numpy()
-                bigan_int_mat[subject-1, d] = (
+                vae_mat[subject, d] = (subject_clf(vae_cf).argmax(1) == subject-1).int().cpu().numpy()
+                bigan_int_mat[subject, d] = (
                             subject_clf(bigan_int).argmax(1) == subject).int().cpu().numpy()
-                vae_int_mat[subject-1, d] = (
+                vae_int_mat[subject, d] = (
                             subject_clf(vae_int).argmax(1) == subject).int().cpu().numpy()
 
     print('BiGAN:', bigan_mat.mean())
